3d89e3e86e25770149ef3cf84f5aa55c799e17b4
Adam Wayland <abwayland@gmail.com>
2022-05-07 23:21:51 -0400

    Add playRound() to javascript.js

--- javascript.js
+++ javascript.js
@@ -7,4 +7,54 @@ function computerPlay() {
     return strArr[index];
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    //if player equals rock
+    if (playerSelection == "rock") {
+        //if computer equals rock
+        if (computerSelection == "rock") {
+            console.log("Player picked rock. Computer picked rock. Game ends in a draw.");
+        }
+        //if computer equals paper
+        if (computerSelection == "paper") {
+            console.log("Player picked rock. Computer picked paper. Computer wins.");
+        }
+        //if computer equals scissors
+        if (computerSelection == "scissors") {
+            console.log("Player picked rock. Computer picked scissors. Player wins.");
+        }
+    //if player equals paper
+    } else if (playerSelection == "paper") {
+        //if computer equals rock
+        if (computerSelection == "rock") {
+            console.log("Player picked paper. Computer picked rock. Player wins.");
+        }
+        //if computer equals paper
+        if (computerSelection == "paper") {
+            console.log("Player picked paper. Computer picked paper. Game ends in a draw.");
+        }
+        //if computer equals scissors
+        if (computerSelection == "scissors") {
+            console.log("Player picked paper. Computer picked scissors. Computer wins.");
+        }
+    //if player equals scissors
+    } else if (playerSelection == "scissors") {
+        //if computer equals rock
+        if (computerSelection == "rock") {
+            console.log("Player picked scissors. Computer picked rock. Computer wins.");
+        }
+        //if computer equals paper
+        if (computerSelection == "paper") {
+            console.log("Player picked scissors. Computer picked paper. Player wins.");
+        }
+        //if computer equals scissors
+        if (computerSelection == "scissors") {
+            console.log("Player picked scissors. Computer picked scissors. Game ends in a draw.");
+        }
+    } else {
+        console.log("Error in playRound()");
+    }
+}
+
+playRound("rock", computerPlay());
